be3dafb6160cf76c7bf9b1cb792919c3591d8f42
Brand0kay <44150429+Brand0kay@users.noreply.github.com>
2018-11-24 13:10:04 -0500

    Add files via upload

---
+++ Models/ParkWeather.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Web.Models
+{
+    public class ParkWeather
+    {
+        public string ParkCode { get; set; }
+        public string ParkName { get; set; }
+        public string State { get; set; }
+        public int Acreage { get; set; }
+        public int ElevationFt { get; set; }
+        public double MilesOfTrail { get; set; }
+        public int NoOfCampsites { get; set; }
+        public string Climate { get; set; }
+        public int YearFounded { get; set; }
+        public int AnlVisitors { get; set; }
+        public string Quote { get; set; }
+        public string QuoteSource { get; set; }
+        public string Description { get; set; }
+        public int EntryFee { get; set; }
+        public int NoOfSpecies { get; set; }
+        public int FiveDayForecast { get; set; }
+        public double LowTemp { get; set; }
+        public double HighTemp { get; set; }
+        public string Forecast { get; set; }
+        public IList<ParkWeather> parkWeathers { get; set; }
+        public string TempUnits { get; set; }
+
+        public void TemperatureConvert()
+        {
+            //if (this.TempUnits == "F")
+            //{
+            //    LowTemp = (LowTemp * 9 / 5) + 32;
+            //    HighTemp = (HighTemp * 9 / 5) + 32
+            //}
+
+
+            if (TempUnits == "C")
+            {
+                LowTemp = (LowTemp - 32) * 5 / 9;
+                HighTemp = (HighTemp - 32) * 5 / 9; 
+            }
+        }
+    }
+}
---
+++ Models/Survey.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Web.Models
+{
+    public class Survey
+    {
+        public int SurveyID { get; set; }
+        public string ParkCode { get; set; }
+        [Required(ErrorMessage ="Email required")]
+        public string Email { get; set; }
+        [Required(ErrorMessage ="State of residence required")]
+        public string State { get; set; }
+        [Required(ErrorMessage ="Activity level required")]
+        public string ActivityLevel { get; set; }
+        public int NoOfSurveys { get; set; }
+        [Required(ErrorMessage ="Park required")]
+        public string NameOfPark { get; set; }
+        [Required(ErrorMessage ="Name required")]
+        public string Name { get; set; }
+
+        public IList<Survey> surveys { get; set; }
+    }
+}
